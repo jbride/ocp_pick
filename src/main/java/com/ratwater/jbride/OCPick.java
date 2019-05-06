7ff27a27fe706e4f5fb0705c3944b5c8d0ef4b92
jbride <jbride2001@yahoo.com>
2019-05-05 19:36:46 -0600

    latest

--- src/main/java/com/ratwater/jbride/OCPick.java
+++ src/main/java/com/ratwater/jbride/OCPick.java
@@ -19,61 +19,78 @@ public class OCPick {
     private static Logger log = LoggerFactory.getLogger("OCPick");
     private static final String YAML_CONFIG_PATH_ENV = "YAML_CONFIG_PATH_ENV";
     private static final String YAML_CONFIG_PATH_ARG = "--config_path=";
+    private static final String HELP_ARG = "--h";
+    private static final String VERSION_ARG = "--version";
     private static final String DEFAULT_CONFIG_FILE_NAME = ".ocp_env_details.yaml";
     private static final String APP_VERSION = "app_version";
     private static String version = "0.0";
     private static Properties appProps = null;
     private static String yamlConfigPath = null;
     private static Map<String, OCPENV> envMap = null;
-
+    
     public static void main(String args[]) {
-        testOC();
         readAppProps();
         determineVariables(args);
         readAndValidateYaml();
         String guid = promptForGuid();
+        testOC();
         login(guid);
     }
-
+    
     private static void testOC() {
-
+        
         InputStream iStream = null;
         try {
             Process p = Runtime.getRuntime().exec("oc version");
             iStream = p.getInputStream();
             String commandOutput = IOUtil.toString(iStream);
-
+            
             log.info("testOC() commandOutput = " + commandOutput);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             if (iStream != null)
-                try { iStream.close();  } catch (IOException e) { e.printStackTrace(); }
+            try { iStream.close();  } catch (IOException e) { e.printStackTrace(); }
         }
     }
-
+    
     private static void determineVariables(String args[]) {
+        
+        String currentUsersHomeDir = System.getProperty("user.home");
+        yamlConfigPath = currentUsersHomeDir + File.separator + DEFAULT_CONFIG_FILE_NAME;
+
         if (args.length > 0) {
             for (int x = 0; x < args.length; x++) {
-                log.info("determineVariables() arg = " + args[x]);
-                if (args[x].startsWith(YAML_CONFIG_PATH_ARG)) {
-                    yamlConfigPath = args[x].substring(13);
+                log.debug("determineVariables() arg = " + args[x]);
+                if(args[x].startsWith(HELP_ARG)) {
+                    dumpHelp();
+                    System.exit(0);
+                } else if (args[x].startsWith(VERSION_ARG)) {
+                    System.out.println("\n\nOC_Pick version = "+version);
+                    System.exit(0);
+                } else if (args[x].startsWith(YAML_CONFIG_PATH_ARG)) {
+                    yamlConfigPath = args[x].substring(14);
+                } else {
+                    log.error("Unknown command line arg: "+args[x]);
+                    dumpHelp();
+                    System.exit(1);
                 }
             }
         }
-        if (StringUtils.isEmpty(yamlConfigPath)) {
-            String temp = System.getenv(YAML_CONFIG_PATH_ENV);
-            if (!StringUtils.isEmpty(temp)) {
-                yamlConfigPath = System.getenv(YAML_CONFIG_PATH_ENV);
-            }
+        if (!StringUtils.isEmpty(System.getenv(YAML_CONFIG_PATH_ENV))) {
+          yamlConfigPath = System.getenv(YAML_CONFIG_PATH_ENV);
         }
-        if (StringUtils.isEmpty(yamlConfigPath)) {
-            String currentUsersHomeDir = System.getProperty("user.home");
-            yamlConfigPath = currentUsersHomeDir + File.separator + DEFAULT_CONFIG_FILE_NAME;
-        }
-        log.info("determineVariables() yaml file to parse = " + yamlConfigPath);
     }
-
+    
+    private static void dumpHelp() {
+        StringBuilder sBuilder = new StringBuilder("OC_PICK Help:");
+        sBuilder.append("\n\n\t"+YAML_CONFIG_PATH_ARG+" : Path to yaml formatted data file containing one or more OCP environments.");
+        sBuilder.append("\n\t\t\t Default path = "+ yamlConfigPath);
+        sBuilder.append("\n\t"+VERSION_ARG+" \t: OC_Pick version");
+        sBuilder.append("\n\n");
+        System.out.println(sBuilder.toString());
+    }
+    
     private static void readAppProps() {
         InputStream is = null;
         try {
@@ -91,19 +108,20 @@ public class OCPick {
         if ((appProps != null) && (!StringUtils.isEmpty((String) appProps.get(APP_VERSION)))) {
             version = (String) appProps.get(APP_VERSION);
         }
-        log.info("main() app version = " + version);
     }
-
+    
     private static void dumpSysProperties() {
         Properties pros = System.getProperties();
         pros.list(System.out);
     }
-
+    
     private static void readAndValidateYaml() {
+        log.info("main() app version = " + version);
         File yamlFile = new File(yamlConfigPath);
         if (!yamlFile.exists())
             throw new RuntimeException("readAndValidateYaml() the following file does not exist: " + yamlConfigPath);
-
+        log.info("determineVariables() yaml file to parse = " + yamlConfigPath);
+        
         FileInputStream yamlReader = null;
         OCPENVs yamlValues = null;
         try {
@@ -157,13 +175,16 @@ public class OCPick {
         StringBuilder lCommand = new StringBuilder("oc login https://");
 
         // 1)  Determine URL to OCP Master
+        String url = null;
         if(ocpEnv.getType().equals(OCPENV.AWS)){
-            lCommand.append("master."+guid+"."+ocpEnv.getSubdomainBase());
+            url = "master."+guid+"."+ocpEnv.getSubdomainBase();
         } else if(ocpEnv.getType().equals(OCPENV.RAVELLO)){
-            lCommand.append(OCPENV.RAVELLO_MASTER_PREFIX+guid+"."+ocpEnv.getSubdomainBase());
+            url = OCPENV.RAVELLO_MASTER_PREFIX+guid+"."+ocpEnv.getSubdomainBase();
         } else {
             throw new RuntimeException("Unknown OCP environment type: "+ocpEnv.getType());
         }
+        lCommand.append(url);
+        testConnectivityToMaster(url);
 
 
         // 2)  Add userId and passwd
@@ -189,6 +210,10 @@ public class OCPick {
             if (iStream != null)
                 try { iStream.close();  } catch (IOException e) { e.printStackTrace(); }
         }
+
+    }
+
+    private static void testConnectivityToMaster(String url) {
 
     }
 }
